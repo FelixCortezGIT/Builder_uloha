d72fff2acba3027410c6f931bf2afadc07f62b83
TomasMacula <tomasmacula@live.com>
2026-02-11 10:19:20 +0100

    vytvoreny __repr__ funkcie teraz nieco aj vracaju

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ class Pasta:
         self.topping = topping
         self.dressing = dressing
         self.chilli = chilli
+    def __repr__(self):
+        return (f"Pasta(type={self.type!r}, sauce={self.sauce!r}, topping={self.topping!r}, dressing={self.dressing!r}, chilli={self.chilli!r})")
 
 class PastaBuilder:
     def __init__(self):
@@ -12,30 +14,35 @@ class PastaBuilder:
         self.sauce = None
         self.topping = None
         self.dressing = None
+        self.chilli = None
     def add_type(self, type):
         self.type = type
+        return self
     def add_sauce(self, sauce):
         self.sauce = sauce
+        return self
     def add_topping(self, topping):
         self.topping = topping
+        return self
     def add_dressing(self, dressing):
         self.dressing = dressing
+        return self
     def add_chilli(self, chilli):
         self.chilli = chilli
+        return self
     def build(self):
         return Pasta(self.type, self.sauce, self.topping, self.dressing, self.chilli)
 
+pasta1 = Pasta('spaghety', 'carbonara', 'parmesan', 'none', 'mild')
+pasta2 = Pasta('tortellini', 'tomato', 'mozzarella', 'none', 'strong')
+pasta3 = Pasta('fusilli', 'pesto', 'parmesan', 'none', 'mild')
 
-pasta1 = Pasta('spaghety', 'carbonara', 'parmesan', False, 'mild')
-pasta2 = Pasta('tortellini', 'tomato', 'mozzarella', False, 'strong')
-pasta3 = Pasta('fusilli', 'pesto', 'parmesan', False, 'mild')
+pasta4 = (PastaBuilder()
+          .add_type("penne")
+          .add_sauce("tartufo")
+          .add_topping("mushrooms")
+          .add_dressing("parmesan")
+          .add_chilli("mild")
+          .build())
 
-pastaBuilder = PastaBuilder()
-pastaBuilder.add_type("penne")
-pastaBuilder.add_sauce("tartufo")
-pastaBuilder.add_topping("mushrooms")
-pastaBuilder.add_dressing("parmesan")
-pastaBuilder.add_chilli("mild")
-
-pasta4 = pastaBuilder.build()
-print(pasta3)
+print(pasta4)
